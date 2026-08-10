438b263b3d28ae3e72f0d65d5657c36a14d0d0dd
Rahul Soni <rahulsoni@Rahuls-MacBook-Air.local>
2026-08-10 18:25:29 +0530

    fix backend

--- models/User.js
+++ models/User.js
@@ -14,7 +14,9 @@ const userSchema = new mongoose.Schema({
     },
     password: {
         type: String,
-        required: true,
+        required: function () {
+            return this.role !== "salesAgent";
+        },
     },
     role: {
         type: String,
